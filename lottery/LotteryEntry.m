5009602ddd7ccf3c3f70101847923f4c9be1af51
Martin Maciaszek <martin@maciaszek.net>
2008-12-26 00:50:32 +0100

    Added description method to LotteryEntry
    The lottery entries can now print themselves

--- lottery/LotteryEntry.m
+++ lottery/LotteryEntry.m
@@ -35,4 +35,13 @@
 {
 	return secondNumber;
 }
+
+- (NSString *)description
+{
+	NSString *result;
+	result = [[NSString alloc] initWithFormat:@"%@ = %d and %d",
+			  [entryDate descriptionWithCalendarFormat:@"%b %d %Y"],
+			  firstNumber, secondNumber];
+	return result;
+}
 @end
