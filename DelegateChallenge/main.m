12e832b523ac25792984960da3c083eed05fb42e
Martin Maciaszek <martin@maciaszek.net>
2008-12-28 18:37:53 +0100

    Time for another challenge
    This time it's a window with a constraint size

---
+++ DelegateChallenge/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DelegateChallenge
+//
+//  Created by Martin Maciaszek on 28.12.08.
+//  Copyright __MyCompanyName__ 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
