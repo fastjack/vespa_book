61eebc68901a8af0d7f63a1f1d704c99fdeb3372
Martin Maciaszek <martin@maciaszek.net>
2008-12-29 02:42:31 +0100

    New Application: RaiseMan

---
+++ RaiseMan/MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  RaiseMan
+//
+//  Created by Martin Maciaszek on 29.12.08.
+//  Copyright __MyCompanyName__ 2008 . All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
