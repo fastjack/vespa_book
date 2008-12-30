3b9c8c894228b105126c840d5746423132abffb0
Martin Maciaszek <martin@maciaszek.net>
2008-12-30 04:53:29 +0100

    Chapter 8, Challenge 2: RaiseMan without bindings

---
+++ RaiseManClassic/MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  RaiseManClassic
+//
+//  Created by Martin Maciaszek on 30.12.08.
+//  Copyright __MyCompanyName__ 2008 . All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
