cad8d38663a5e9a99795955db10ffe41966e70d3
Martin Maciaszek <martin@maciaszek.net>
2009-01-01 22:45:00 +0100

    Added undo for adding and deleting people

--- RaiseMan/MyDocument.h
+++ RaiseMan/MyDocument.h
@@ -8,10 +8,13 @@
 
 
 #import <Cocoa/Cocoa.h>
+@class Person;
 
 @interface MyDocument : NSDocument
 {
 	NSMutableArray *employees;
 }
 - (void)setEmployees:(NSMutableArray *)a;
+- (void)removeObjectFromEmployeesAtIndex:(int)index;
+- (void)insertObject:(Person *)p inEmployeesAtIndex:(int)index;
 @end
--- RaiseMan/MyDocument.m
+++ RaiseMan/MyDocument.m
@@ -7,6 +7,7 @@
 //
 
 #import "MyDocument.h"
+#import "Person.h"
 
 @implementation MyDocument
 
@@ -33,6 +34,35 @@
 	[a retain];
 	[employees release];
 	employees = a;
+}
+
+- (void)insertObject:(Person *)p inEmployeesAtIndex:(int)index
+{
+	NSLog(@"addint %@ to %@", p, employees);
+	// Add the inverse of this operation to the undo stack
+	NSUndoManager *undo = [self undoManager];
+	[[undo prepareWithInvocationTarget:self]
+		removeObjectFromEmployeesAtIndex:index];
+	if (![undo isUndoing]) {
+		[undo setActionName:@"Insert Person"];
+	}
+	// Add the Person to the array
+	[employees insertObject:p atIndex:index];
+}
+
+- (void)removeObjectFromEmployeesAtIndex:(int)index
+{
+	Person *p = [employees objectAtIndex:index];
+	NSLog(@"removing %@ from %@", p, employees);
+	// Add the inverse of this operation to the undo stack
+	NSUndoManager *undo = [self undoManager];
+	[[undo prepareWithInvocationTarget:self] insertObject:p
+												  inEmployeesAtIndex:index];
+	if (![undo isUndoing]) {
+		[undo setActionName:@"Delete Person"];
+	}
+	
+	[employees removeObjectAtIndex:index];
 }
 
 - (NSString *)windowNibName
