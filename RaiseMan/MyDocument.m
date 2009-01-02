bb191027b5cc5bd0425d3afba13ef77d53b9a4a4
Martin Maciaszek <martin@maciaszek.net>
2009-01-02 07:00:19 +0100

    Added saving and loading capabilities

--- RaiseMan/MyDocument.m
+++ RaiseMan/MyDocument.m
@@ -171,30 +171,32 @@
 
 - (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError
 {
-    // Insert code here to write your document to data of the specified type. If the given outError != NULL, ensure that you set *outError when returning nil.
-
-    // You can also choose to override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-
-    // For applications targeted for Panther or earlier systems, you should use the deprecated API -dataRepresentationOfType:. In this case you can also choose to override -fileWrapperRepresentationOfType: or -writeToFile:ofType: instead.
-
-    if ( outError != NULL ) {
-		*outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:NULL];
-	}
-	return nil;
+	// End editing
+	[[tableView window] endEditingFor:nil];
+	
+	// Create an NSData object from the employees array
+	return [NSKeyedArchiver archivedDataWithRootObject:employees];
 }
 
 - (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError
 {
-    // Insert code here to read your document from the given data of the specified type.  If the given outError != NULL, ensure that you set *outError when returning NO.
-
-    // You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead. 
-    
-    // For applications targeted for Panther or earlier systems, you should use the deprecated API -loadDataRepresentation:ofType. In this case you can also choose to override -readFromFile:ofType: or -loadFileWrapperRepresentation:ofType: instead.
-    
-    if ( outError != NULL ) {
-		*outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:NULL];
+	NSLog(@"About to read data of type %@", typeName);
+	NSMutableArray *newArray = nil;
+	@try {
+		newArray = [NSKeyedUnarchiver unarchiveObjectWithData:data];
 	}
-    return YES;
+	@catch (NSException * e) {
+		if (outError) {
+			NSDictionary *d = [NSDictionary dictionaryWithObject:@"The data is corrupted." 
+														  forKey:NSLocalizedFailureReasonErrorKey];
+			*outError = [NSError errorWithDomain:NSOSStatusErrorDomain
+											code:unimpErr
+										userInfo:d];
+		}
+		return NO;
+	}
+	[self setEmployees:newArray];
+	return YES;
 }
 
 @end
