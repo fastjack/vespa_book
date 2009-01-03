e82321ca1778974c51992e3845abb7344baba97c
Martin Maciaszek <martin@maciaszek.net>
2009-01-03 02:07:52 +0100

    draw a path in the view

--- ImageFun/StretchView.h
+++ ImageFun/StretchView.h
@@ -10,7 +10,7 @@
 
 
 @interface StretchView : NSView {
-
+	NSBezierPath *path;
 }
-
+- (NSPoint)randomPoint;
 @end
--- ImageFun/StretchView.m
+++ ImageFun/StretchView.m
@@ -12,17 +12,51 @@
 @implementation StretchView
 
 - (id)initWithFrame:(NSRect)frame {
-    self = [super initWithFrame:frame];
-    if (self) {
-        // Initialization code here.
+    if (![super initWithFrame:frame]) {
+        return nil;
     }
+	
+	// Seed the random number generator
+	srandom(time(NULL));
+	
+	// Create a path object
+	path = [[NSBezierPath alloc] init];
+	[path setLineWidth:3.0];
+	NSPoint p = [self randomPoint];
+	[path moveToPoint:p];
+	int i;
+	for (i = 0; i < 15; i++) {
+		p = [self randomPoint];
+		[path lineToPoint:p];
+	}
+	[path closePath];
     return self;
+}
+
+- (void)dealloc
+{
+	[path release];
+	[super dealloc];
+}
+
+// randomPoint returns a random point inside the view
+- (NSPoint)randomPoint
+{
+	NSPoint result;
+	NSRect r = [self bounds];
+	result.x = r.origin.x + random() % (int)r.size.width;
+	result.y = r.origin.y + random() % (int)r.size.height;
+	return result;
 }
 
 - (void)drawRect:(NSRect)rect {
     NSRect bounds = [self bounds];
 	[[NSColor greenColor] set];
 	[NSBezierPath fillRect:bounds];
+	
+	// Draw the path in white
+	[[NSColor whiteColor] set];
+	[path stroke];
 }
 
 @end
