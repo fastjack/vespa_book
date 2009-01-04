d781cf16abe30e61945e210c4e3152a893998592
Martin Maciaszek <martin@maciaszek.net>
2009-01-04 10:04:43 +0100

    A new challenge

---
+++ MiniIllustrator/MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  MiniIllustrator
+//
+//  Created by Martin Maciaszek on 04.01.09.
+//  Copyright Martin Maciaszek 2009 . All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
