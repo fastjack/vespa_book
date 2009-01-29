8e3a009d39247dd810ddaa74d9e61a5d55a81908
Martin Maciaszek <martin@maciaszek.net>
2009-01-29 16:12:15 +0100

    A new core data document-based application

---
+++ Departments/MyDocument.h
@@ -0,0 +1,14 @@
+//
+//  MyDocument.h
+//  Departments
+//
+//  Created by Martin Maciaszek on 29.01.09.
+//  Copyright __MyCompanyName__ 2009 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSPersistentDocument {
+}
+
+@end
